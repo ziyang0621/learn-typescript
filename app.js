29e917ddd3ce117b6a404a688038568147b8005c
ziyang <ziyang0621@gmail.com>
2018-11-28 07:47:18 -0800

    update

--- app.js
+++ app.js
@@ -31,3 +31,66 @@ var car = 'BMW';
 console.log(car);
 car = { brand: 'BMW', series: 3 };
 console.log(car);
+// functions
+function returnMyName() {
+    return myName;
+}
+console.log(returnMyName());
+// void
+function sayHello() {
+    console.log("Hello!");
+}
+// argument types
+function multiply(value1, value2) {
+    return value1 * value2;
+}
+// console.log(multiply(2, 'Max'));
+console.log(multiply(10, 2));
+// function types
+var myMultiply;
+// myMultiply = sayHello;
+// myMultiply();
+myMultiply = multiply;
+console.log(myMultiply(5, 2));
+// objects
+var userData = {
+    name: "Max",
+    age: 27
+};
+// userData = {
+//     a: 'Hello',
+//     b: 22
+// };
+// complex object
+var complex = {
+    data: [100, 3.99, 10],
+    output: function (all) {
+        return this.data;
+    }
+};
+var complex2 = {
+    data: [100, 3.99, 10],
+    output: function (all) {
+        return this.data;
+    }
+};
+// union types
+var myRealRealAge = 27;
+myRealRealAge = '27';
+// myRealRealAge = true;
+// check types
+var finalValue = 30;
+if (typeof finalValue == 'number') {
+    console.log('Final value is a number');
+}
+// never
+function neverReturns() {
+    throw new Error('An error!');
+}
+// Nullable types
+var canBeNull = 12;
+canBeNull = null;
+var canAlsoBeNull;
+canAlsoBeNull = null;
+var canThisBeAny = null;
+canThisBeAny = 12;
