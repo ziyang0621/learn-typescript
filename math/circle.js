58326f279c1a69d5e12e8800a97bb697adfe936e
ziyang <ziyang0621@gmail.com>
2018-12-11 20:10:44 -0800

    section 6

---
+++ math/circle.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.PI = 3.14;
+function calculateCirumcumference(diameter) {
+    return diameter * exports.PI;
+}
+exports.calculateCirumcumference = calculateCirumcumference;
