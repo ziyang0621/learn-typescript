1a2bcaa3d6500299d31838b220ea6508408b7e06
ziyang <ziyang0621@gmail.com>
2019-02-13 20:06:31 -0800

    section 11 webpack setup

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    entry:"./app.ts",
+    output: {
+        filename:"./bundle.js"
+    },
+    devtool:"source-map",
+    resolve: {
+        extensions: ["*",".ts",".tsx",".js"]
+    },
+    module:{
+        rules: [
+            {test:/\.tsx?$/, loader:"ts-loader"}
+        ]
+    }
+};
